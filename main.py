94c64e0a9edd902c2ef6652387c73e083b6c1f14
Yashwanth <yash.nth19@gmail.com>
2022-01-20 20:55:21 +0530

    proper message

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running and I am changing something."
+    return "Flask app is running and I am changing something very bad."
 
 port = int(os.getenv("PORT", 5001))
 
